4535c015b46b30d3cab03fb2ce8845dd60da4e03
arkenoi <arkenoi>
2005-06-16 18:57:51 +0000

    Gone for good

--- UIT/go.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-export GENERICHOME=/usr/installed2/UITV3/Generic  
-export UITHOME=/usr/installed2/UITV3/UIT  
-export SLINGSHOT=SLINGSHOT  
-export SSPKGHOME=/usr/openwin  
-make world_g++_NO_XVPS
