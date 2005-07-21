cdb15d2cf8cf82a9cbe33f781e245c098a5135e5
arkenoi <arkenoi>
2005-07-21 14:22:40 +0000

    Initial revision

---
+++ xvmoontool/config.h
@@ -0,0 +1,25 @@
+/* 	Set up geographic coordinates (Longitude, Latitude)
+	of our home. Ask your local airport or look in a
+	good map of your country.
+*/
+
+/* coordinates of Berlin, Germany, [13-24'-00"E x 52-30'-00"N] */
+
+#define	LONG_DEGREES	-13
+#define	LONG_MINUTES	-24
+#define	LONG_SECONDS	-0
+
+#define	LAT_DEGREES	52
+#define	LAT_MINUTES	30
+#define	LAT_SECONDS	0
+
+/* Yosemite National Park, California, USA, [119-30'-00"W x 38-00'-00"N]*/
+/*
+ * #define LONG_DEGREES    119
+ * #define LONG_MINUTES    30
+ * #define LONG_SECONDS    0
+ *
+ * #define LAT_DEGREES     38
+ * #define LAT_MINUTES     0
+ * #define LAT_SECONDS     0
+*/
---
+++ xvmoontool/riseset.h
@@ -0,0 +1,44 @@
+/*
+ * $Id: riseset.h,v 1.1 2005/07/21 14:22:40 arkenoi Exp $
+ */
+#ifndef RISESET_H
+#define RISESET_H
+
+/*
+ * calentool - day/week/month/year-at-a-glance calendar for XView/Open Look
+ * 
+ * Copyright 1988, 1989, 1991, 1994 by Tektronix, Inc. - All Rights Reserved.
+ * 
+ * Permission to use, copy, modify, and distribute this software and its
+ * documentation for any purpose is hereby granted without fee, provided that
+ * the above copyright notice appear in all copies and that both that
+ * copyright notice and this permission notice appear in supporting
+ * documentation, and that the name of Tektronix, Inc. not be used in
+ * advertising or publicity pertaining to distribution of the software
+ * without specific, written prior permission.
+ * 
+ * TEKTRONIX INCORPORATED MAKES NO REPRESENTATIONS ABOUT THE
+ * SUITABILITY OF THIS SOFTWARE FOR ANY PURPOSE.  IT IS PROVIDED "AS IS"
+ * WITHOUT EXPRESS OR IMPLIED WARRANTY.  TEKTRONIX INCORPORATED
+ * DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.  IN NO
+ * EVENT SHALL TEKTRONIX INCORPORATED BE LIABLE FOR ANY SPECIAL,
+ * INDIRECT OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM
+ * LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+ * OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR
+ * PERFORMANCE OF THIS SOFTWARE.
+ * 
+ * Author: Bill Randle, Tektronix, Inc. <billr@saab.cna.tek.com>
+ */ 
+
+/*
+ * index into riseset_buf[]
+ */
+#define	B_MRD	0	/* Moon Rise Today */
+#define	B_MSD	1	/* Moon Set Today */
+#define	B_MRT	2	/* Moon Rise Tomorrow */
+#define	B_MST	3	/* Moon Set Tomorrow */
+
+#define B_SIZE	(B_LOD+1)
+
+#endif
